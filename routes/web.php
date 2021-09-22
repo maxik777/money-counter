8d42a97bee35500c892373273f8fa60cff4fdd9e
maxik777 <maxdeveloped88@gmail.com>
2021-09-22 08:39:26 +0300

    configure app

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,8 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
